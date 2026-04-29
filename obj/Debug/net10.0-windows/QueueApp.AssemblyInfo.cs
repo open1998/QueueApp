d27437602844b49332dbf8ee3e7b860f6779161b
fareeh <ahamedfareeh@gmail.com>
2026-04-29 15:57:53 +0530

    last

--- obj/Debug/net10.0-windows/QueueApp.AssemblyInfo.cs
+++ obj/Debug/net10.0-windows/QueueApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("QueueApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1c98aa7892c01c282cd4825390fd7cd11fab783e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+833978ed0e5376991acbbbe6b61720e633f9d287")]
 [assembly: System.Reflection.AssemblyProductAttribute("QueueApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("QueueApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
